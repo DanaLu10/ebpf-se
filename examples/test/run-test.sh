777f70f0cce35fc23f125952f55c543a139fb2e1
Dana Lu <dl1820@ic.ac.uk>
2024-05-19 10:49:55 +0100

    Two program: implementation & test

--- examples/test/run-test.sh
+++ examples/test/run-test.sh
@@ -2,9 +2,19 @@
 
 # exec > /dev/null 2>&1
 
-find . -type f -name Makefile -execdir make verify > /dev/null 2>&1 \;
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# find . -type f -name Makefile -execdir make verify > /dev/null 2>&1 \;
 
 for d in $(find . -mindepth 1 -maxdepth 1 -type d) ; do 
-  cd "$d" && cmp --silent expected klee-last/verification || echo "files are different for $d"; 
+  if [[ $d == ./two-phase* ]] ;
+  then
+    echo "running two phase for $d"
+    cd "$d" && make verify-two-phase > /dev/null 2>&1 && cmp --silent expected klee-last/overlap || echo -e  "${RED}files are different for $d${NC}";
+  else
+    echo "running normal for $d"
+    cd "$d" && make verify > /dev/null 2>&1 && cmp --silent expected klee-last/verification || echo -e "${RED}files are different for $d${NC}"; 
+  fi
   cd ../
 done
---
+++ verification/verification_helpers.h
@@ -0,0 +1,9 @@
+#ifndef __VERIFICATION_HELPER
+#define __VERIFICATION_HELPER
+
+static void __separate() {
+  // do nothing. This function is used as a separator for running two programs
+}
+
+
+#endif
